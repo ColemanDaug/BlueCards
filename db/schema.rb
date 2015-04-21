7e1bb2fe1295fbd1caa3b8b7417460bae5700148
Jim-Daugherty <jim@daughouse.com>
2015-04-20 21:46:48 -0500

    Layout for User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402162035) do
+ActiveRecord::Schema.define(version: 20150421023903) do
 
   create_table "blue_cards", force: true do |t|
     t.string   "name"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20150402162035) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "trooptype"
+    t.string   "troopnumber"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
