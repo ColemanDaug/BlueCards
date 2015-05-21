06a1a838ac954967346a1e950ad939bc7e20c286
Coleman Daugherty <coleman@trooptrack.com>
2015-05-21 11:52:30 -0500

    Fix for the Edit Profile Page
    Quick fix for the page

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150421023903) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.integer  "unit_number",            default: 0,  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
