d820068e0a0bfa163b5ea661558438d41b35e2de
Jim-Daugherty <jim@daughouse.com>
2015-05-21 22:05:39 -0500

    Latest crap

--- app/controllers/bluecards_controller.rb
+++ app/controllers/bluecards_controller.rb
@@ -1,12 +1,28 @@
 class BluecardsController < ApplicationController
 
+  #def index
+    #@bluecards = BlueCard.where(user_id: current_user.id)
+  #end
+
   def new
+    @bluecards = BlueCard.where(user_id: current_user.id)
+    @currentuser = current_user
+  end
+
+  def show
+
+  end
+
+  def create
+
+    @bluecard = BlueCard.new(bluecard_params)
+    @bluecard.user_id = current_user.id
+    @bluecard.save
+    redirect_to  :back
   end
 
   def bluecard_params
-    params.require(:bluecard).permit(:name, :address, :city, :unit, :district, :council, :date)
+    params.require(:bluecard).permit(:scout, :street, :city, :unit, :district, :council, :date)
   end
-
-
 
 end
